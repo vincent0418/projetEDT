be2ffadf4269b99e00212ec2fbf8f860d39a2c87
Vincent Audigier <vincent-audigier@outlook.fr>
2018-10-06 19:47:55 +0200

    adding display & bug fix
    addign display for more understanding
    fix : a class could take place during two days

--- src/activity/Subject.java
+++ src/activity/Subject.java
@@ -13,11 +13,17 @@ public class Subject{
 		this.duration = time;
 	}
 	
-	public IntVar getHour() {
+	public IntVar getIntvar() {
 		return this.hour;
 	}
 	
 	public int getDuration() {
 		return this.duration;
+	}
+	
+	public String toString() {
+		double hStart = ((double) hour.getValue() % 1000 + 800) / 100;
+		double hEnd = ((double) hour.getValue() % 1000 + 800 + this.duration) / 100;
+		return hour.getName() + ": " + hStart + "h - " + hEnd + "h";
 	}
 }
